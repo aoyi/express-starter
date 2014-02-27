bf4ad7d13f487af721a82126681de09420e1c8bf
Aoyi Shan <aoyi95@berkeley.edu>
2014-02-26 20:10:06 -0800

    completed

--- static/js/imgrr.js
+++ static/js/imgrr.js
@@ -1,17 +1,38 @@
 // Problem 2 (Peekaboo) ------------------------------------------------------
 // WRITE CODE HERE
-$('#Go_Away!').click(function() {
-	$('#gorilla_jpg').hide();
-})
+
+
+
+$('#toggle_img').click(function() {
+	if ($('#toggle_img').text() === "Go Away!") {
+		$('#main_img').hide();
+		$('#toggle_img').text('Come back!');
+	} else {
+		$('#main_img').show();
+		$('#toggle_img').text('Go Away!');
+	}
+});
 
 
 // Problem 3 (Swap Em) -----------------------------------------------
 // WRITE CODE HERE
 
+$('#change_img').click(function() {
+
+	alert('/static/img/' + $('#new_img_file').val());
+	$('#main_img').attr('src','/static/img/' + $('#new_img_file').val());
+})
+
 // Problem 4 (Find the Source) -------------------------------------------------
 $('.clickable').click(function() {
-  // WRITE CODE HERE
+  $(this).attr('width','200px');
+  alert($(this).attr('src'));
 });
 
 // Problem 5 (Imgrr) -------------------------------------------------
 // WRITE CODE HERE
+
+$('.可点击的').click(function() {
+	$('#second_img').attr('src',$(this).attr('src'));
+});
+
