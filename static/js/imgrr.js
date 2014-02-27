8f2be270482b5a6a68fab1c91f57c7fc1265e330
Aoyi Shan <aoyi95@berkeley.edu>
2014-02-26 18:42:19 -0800

    lll

---
+++ static/js/imgrr.js
@@ -0,0 +1,17 @@
+// Problem 2 (Peekaboo) ------------------------------------------------------
+// WRITE CODE HERE
+$('#Go_Away!').click(function() {
+	$('#gorilla_jpg').hide();
+})
+
+
+// Problem 3 (Swap Em) -----------------------------------------------
+// WRITE CODE HERE
+
+// Problem 4 (Find the Source) -------------------------------------------------
+$('.clickable').click(function() {
+  // WRITE CODE HERE
+});
+
+// Problem 5 (Imgrr) -------------------------------------------------
+// WRITE CODE HERE
