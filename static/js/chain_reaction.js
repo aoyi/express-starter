47706838b3bb934ce48ee0577d8940e87ad59c4e
Aoyi Shan <aoyi95@berkeley.edu>
2014-04-13 18:06:26 -0700

    balls

--- static/js/chain_reaction.js
+++ static/js/chain_reaction.js
@@ -8,33 +8,41 @@ $(document).ready(function() {
   // PUT STUFF HERE
   var balls = []
   var b0 = {
-    xCoor: 20,
-    yCoor: 20,
+    xCoor: 50,
+    yCoor: 250,
     radius: 20,
     vx: 5,
     vy:5
   };
     var b1 = {
-    xCoor: 140,
-    yCoor: 140,
+    xCoor: 300,
+    yCoor: 400,
     radius: 50,
     vx: 5,
     vy:5
   };
     var b2 = {
-    xCoor: 300,
-    yCoor: 300,
+    xCoor: 200,
+    yCoor: 90,
     radius: 80,
     vx: 5,
     vy:5
   };
 
+
   balls.push(b0); 
   balls.push(b1);
   balls.push(b2);
   // Run an interation of the game
+
+
   var updateGame = function() {
+
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+
     for (var i = 0; i < balls.length; i++) {
+
     context.beginPath();
     context.arc(balls[i].xCoor,balls[i].yCoor, balls[i].radius ,0,2*Math.PI);
     context.fillStyle = 'pink';
@@ -42,6 +50,28 @@ $(document).ready(function() {
     context.closePath();
     context.stroke();
     };
+    setTimeout(updateGame,10);
+
+    for (var i = 0; i < balls.length; i++) {
+    var xx = function() {
+    balls[i].xCoor = balls[i].xCoor + balls[i].vx;
+  }
+    var yy = function() {
+    balls[i].yCoor = balls[i].yCoor + balls[i].vy;
+  }
+    xx();
+    yy();
+
+    
+    if (balls[i].xCoor > width - 20 )
+    { balls[i].vx = -balls[i].vx };
+     if (balls[i].xCoor < 20 )
+    { balls[i].vx = -balls[i].vx };   
+    if (balls[i].yCoor > height - 20)
+    { balls[i].vy = -balls[i].vy };
+    if (balls[i].yCoor < 20)
+    { balls[i].vy = -balls[i].vy };
+  };
   };
 
   // Handle a canvas click event
